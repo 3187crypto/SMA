a997d8093ebeb9d3f13b41c4b14e9eede08ec58d
您的名字 <>
2026-03-29 10:43:42 +0800

    修复 ENS 解析错误：在 index.js 中禁用 ENS

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Web3ReactProvider } from '@web3-react/core';
-import { ethers } from 'ethers';
 import './index.css';
 import App from './App';
+import { ethers } from 'ethers';
 
-const getLibrary = (provider) => {
-  return new ethers.providers.Web3Provider(provider);
+// 禁用 ENS 解析（BSC 网络不支持 ENS）
+const originalGetAddress = ethers.utils.getAddress;
+ethers.utils.getAddress = (address) => {
+  // 如果是地址格式，直接返回，不进行 ENS 解析
+  if (address && address.startsWith('0x')) {
+    return address;
+  }
+  return originalGetAddress(address);
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <App />
-    </Web3ReactProvider>
+    <App />
   </React.StrictMode>
 );
