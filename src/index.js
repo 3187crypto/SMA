e3d6af512afd25bfeef66dc568e064a50fd3e5bb
您的名字 <>
2026-03-29 10:46:40 +0800

    修复 ENS 错误：在 Web3ReactProvider 中禁用 ENS 解析

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Web3ReactProvider } from '@web3-react/core';
 import { ethers } from 'ethers';
 
-// 禁用 ENS 解析（BSC 网络不支持 ENS）
-const originalGetAddress = ethers.utils.getAddress;
-ethers.utils.getAddress = (address) => {
-  // 如果是地址格式，直接返回，不进行 ENS 解析
-  if (address && address.startsWith('0x')) {
-    return address;
-  }
-  return originalGetAddress(address);
-};
+// 自定义库函数，禁用 ENS
+function getLibrary(provider) {
+  const library = new ethers.providers.Web3Provider(provider);
+  // 禁用 ENS 解析（BSC 网络不支持）
+  library.ensAddress = null;
+  return library;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>
 );
